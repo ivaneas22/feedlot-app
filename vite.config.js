e1ff83c0bb090465dcc461116bd231bebf9417a5
ivaneas22 <iva.eq.iq@gmail.com>
2025-11-12 10:18:23 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/feedlot-app/',   // <- ruta del repo para GH Pages
   plugins: [react()],
-  base: '/feedlot-app/', // 👈 importante: mismo nombre que el repo
 })
