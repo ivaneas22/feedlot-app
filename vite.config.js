e658016edf6be21f5dae98e8928c469082934839
ivaneas22 <iva.eq.iq@gmail.com>
2025-11-12 09:48:11 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/feedlot-app/', // 👈 importante: mismo nombre que el repo
 })
